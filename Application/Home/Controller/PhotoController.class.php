fafced758aa5b1b35dbdb5633a62b177e5993115
Min Wang <yinger650@gmail.com>
2016-05-13 01:27:38 +0800

    implement the public and private photo display

--- Application/Home/Controller/PhotoController.class.php
+++ Application/Home/Controller/PhotoController.class.php
@@ -23,7 +23,107 @@ class PhotoController extends Controller
     */
 
     public function upload() {
+        $album = M('Album');
+        $list = $album->where('owner = '.session('user')['uid'])->select();
+        $this->assign('album',$list);
         $this->display();
+    }
+
+    public function callback(){
+
+        // 1.获取OSS的签名header和公钥url header
+        $authorizationBase64 = "";
+        $pubKeyUrlBase64 = "";
+        /*
+         * 注意：如果要使用HTTP_AUTHORIZATION头，你需要先在apache或者nginx中设置rewrite，以apache为例，修改
+         * 配置文件/etc/httpd/conf/httpd.conf(以你的apache安装路径为准)，在DirectoryIndex index.php这行下面增加以下两行
+            RewriteEngine On
+            RewriteRule .* - [env=HTTP_AUTHORIZATION:%{HTTP:Authorization},last]
+         * */
+        if (isset($_SERVER['HTTP_AUTHORIZATION']))
+        {
+            $authorizationBase64 = $_SERVER['HTTP_AUTHORIZATION'];
+        }
+        if (isset($_SERVER['HTTP_X_OSS_PUB_KEY_URL']))
+        {
+            $pubKeyUrlBase64 = $_SERVER['HTTP_X_OSS_PUB_KEY_URL'];
+        }
+
+        if ($authorizationBase64 == '' || $pubKeyUrlBase64 == '')
+        {
+            header("http/1.1 403 Forbidden");
+            exit();
+        }
+
+    // 2.获取OSS的签名
+        $authorization = base64_decode($authorizationBase64);
+
+    // 3.获取公钥
+        $pubKeyUrl = base64_decode($pubKeyUrlBase64);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $pubKeyUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        $pubKey = curl_exec($ch);
+        if ($pubKey == "")
+        {
+            //header("http/1.1 403 Forbidden");
+            exit();
+        }
+
+    // 4.获取回调body
+        $body = file_get_contents('php://input');
+        $bodyArray = explode("&", $body);
+        $callbackData = array();
+        foreach ($bodyArray as $item) {
+            $itemData = explode("=",$item);
+            $callbackData[$itemData[0]] = $itemData[1];
+        }
+        $photo = M('Photo');
+        $album = M('Album');
+        $condition['owner'] = $callbackData['uploader'];
+        $condition['title'] = $callbackData['album'];
+        $aid = $album->where($condition)->find();
+
+        $data['oss_url'] = $callbackData['filename'];
+        $data['uploader'] = $callbackData['uploader'];
+        $data['time'] = date('Y-m-d H:i:s',NOW_TIME);
+        $data['album'] = $aid['aid'];
+        $data['description'] = $callbackData['description'];
+        $data['public'] = ($callbackData['public']  == "true");
+        $status = $photo->data($data)->add();
+
+
+            // 5.拼接待签名字符串
+        $authStr = '';
+        $path = $_SERVER['REQUEST_URI'];
+        $pos = strpos($path, '?');
+        if ($pos === false)
+        {
+            $authStr = urldecode($path)."\n".$body;
+        }
+        else
+        {
+            $authStr = urldecode(substr($path, 0, $pos)).substr($path, $pos, strlen($path) - $pos)."\n".$body;
+        }
+
+    // 6.验证签名
+        $ok = openssl_verify($authStr, $authorization, $pubKey, OPENSSL_ALGO_MD5);
+        if ($ok == 1)
+        {
+            header("Content-Type: application/json");
+            $data = array("Status"=>$status,
+                          "albumID"=>$aid['aid'],
+                          "file" => $callbackData['filename'],
+                          "public" => $callbackData['public'],
+                );
+            echo json_encode($data);
+        }
+        else
+        {
+            //header("http/1.1 403 Forbidden");
+            exit();
+        }
     }
 
     public function oss() {
@@ -39,12 +139,12 @@ class PhotoController extends Controller
         if (!is_login()) exit('unauthorized!');
         $id = 'ulbXmLMev4vDnlFt';
         $key = 'W0MEvHv6GCMhElfrxGRxKr3c1Npvrm';
-        $host = 'http://alialbum.oss-cn-qingdao.aliyuncs.com';
+        $host = 'https://alialbum.oss-cn-qingdao.aliyuncs.com';
         $callbackUrl = U('callback','','',true);
         $callback_param = array('callbackUrl'=>$callbackUrl,
-            'callbackBody'=>'filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}',
-            'callbackBodyType'=>"application/x-www-form-urlencoded",
-            'uploadUser'=>session('user')['uid']);
+            'callbackBody'=>'filename=${object}&size=${size}&mimeType=${mimeType}&height=${imageInfo.height}&width=${imageInfo.width}&uploader='
+                            .session('user')['uid'].'&album=${album}&description=${description}&public=${public}',
+            'callbackBodyType'=>"application/x-www-form-urlencoded");
         $callback_string = json_encode($callback_param);
 
         $base64_callback_body = base64_encode($callback_string);
@@ -53,7 +153,7 @@ class PhotoController extends Controller
         $end = $now + $expire;
         $expiration = gmt_iso8601($end);
 
-        $dir = '/Public/img';
+        $dir = '';
 
         //最大文件大小.用户可以自己设置
         $condition = array(0=>'content-length-range', 1=>0, 2=>1048576000);
@@ -89,6 +189,21 @@ class PhotoController extends Controller
         $album = M('Album');
         $list = $album->where('owner = '.session('user')['uid'])->select();
         $this->assign('list',$list);
+        $photo = M('Photo');
+        $publicPhoto = $photo->where('public = true')->select();
+        $this->assign('publicPhoto',$publicPhoto);
+        if (IS_POST) {
+            $albumName = I('post.album');
+            $condition['owner'] = session('user')['uid'];
+            $condition['title'] = $albumName;
+            $aid = $album->where($condition)->find();
+            $aid = $aid['aid'];
+            if ($aid != NULL) {
+                $photo = M('Photo');
+                $privatePhoto = $photo->where('album = '.$aid)->select();
+                $this->assign('privatePhoto',$privatePhoto);
+            }
+        }
         $this->display();
     }
 
--- ThinkPHP/ThinkPHP.php
+++ ThinkPHP/ThinkPHP.php
@@ -82,8 +82,10 @@ if (!IS_CLI) {
     if (!defined('_PHP_FILE_')) {
         if (IS_CGI) {
             //CGI/FASTCGI模式下
-            $_temp = explode('.php', $_SERVER['PHP_SELF']);
-            define('_PHP_FILE_', rtrim(str_replace($_SERVER['HTTP_HOST'], '', $_temp[0] . '.php'), '/'));
+            //$_temp = explode('.php', $_SERVER['PHP_SELF']);
+
+            //define('_PHP_FILE_', rtrim(str_replace($_SERVER['HTTP_HOST'], '', $_temp[0] . '.php'), '/'));
+            define('_PHP_FILE_', rtrim($_SERVER['SCRIPT_NAME'], '/'));
         } else {
             define('_PHP_FILE_', rtrim($_SERVER['SCRIPT_NAME'], '/'));
         }
@@ -91,6 +93,7 @@ if (!IS_CLI) {
     if (!defined('__ROOT__')) {
         $_root = rtrim(dirname(_PHP_FILE_), '/');
         define('__ROOT__', (('/' == $_root || '\\' == $_root) ? '' : $_root));
+
     }
 }
 
